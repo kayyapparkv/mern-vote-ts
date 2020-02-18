0830a84605b54e29b1a4b709aad8c38f8de58b91
kayyapparkv <kayyapparkv@gmail.com>
2020-02-18 19:52:38 +0530

    made changes to poll model for type script compatibility by adding interfaces and etending to the mongoose document

--- src/model/poll.ts
+++ src/model/poll.ts
@@ -1,4 +1,19 @@
-import mongoose, { mongo } from 'mongoose';
+import mongoose from 'mongoose';
+
+interface OptionSchema extends mongoose.Document {
+    option: string;
+    votes: number;
+}
+
+export interface PollSchemaDocument extends mongoose.Document {
+    _doc: OptionSchema[];
+    user: any;
+    question: string;
+    options: OptionSchema[];
+    voted: any[];
+    created: Date;
+
+}
 
 const optionSchema = new mongoose.Schema({
     option: String,
@@ -25,4 +40,6 @@ const pollSchema = new mongoose.Schema({
     }
 });
 
-export default mongoose.model('Poll', pollSchema);
+const poll = mongoose.model<PollSchemaDocument>('Poll', pollSchema);
+
+export default poll;
