0dfcae6554f25ffd3b13ec752408782d400dcd2e
kayyapparkv <kayyapparkv@gmail.com>
2020-02-18 19:54:30 +0530

    written create poll method for poll in  controller

--- src/controller/poll.ts
+++ src/controller/poll.ts
@@ -1,9 +1,38 @@
 import { Request, Response, NextFunction } from 'express';
 
+import db from '../model';
 
 
-export const nothing = async(req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({
-        'ok': 'success'
-    });
+const createPoll = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const {id} = (<any>req).decoded;
+
+        const {question, options} = req.body;
+
+        const user = await db.User.findById(id);
+
+        const poll = await db.Poll.create({
+            user,
+            question,
+            options: options.map((option: any) => ({
+                option,
+                votes: 0
+            }))
+        });
+
+        user.polls.push(poll._id);
+        await user.save();
+
+        res.status(201).json({
+            question: poll.question,
+            answer: poll.options,
+            user: user._id
+        });
+    } catch (err) {
+        return next(err);
+    }
+};
+
+export default {
+    createPoll
 };
